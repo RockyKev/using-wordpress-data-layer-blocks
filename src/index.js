08ee1eb0b8e56d29b907bd10616b86f52182cae6
Rocky Kev <6101650+RockyKev@users.noreply.github.com>
2023-05-20 19:10:46 -0700

    working search component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,69 @@
-import { render } from "@wordpress/element";
+import { SearchControl, Spinner } from "@wordpress/components";
+import { useState, render } from "@wordpress/element";
 import { useSelect } from "@wordpress/data";
 import { store as coreDataStore } from "@wordpress/core-data";
-import { decodeEntities } from '@wordpress/html-entities';
+import { decodeEntities } from "@wordpress/html-entities";
 
 function MyFirstApp() {
-  //   const pages = [{ id: "mock", title: "The Sample Page?" }];
-  const pages = useSelect((select) => select(coreDataStore).getEntityRecords("postType", "page"), []);
-  console.log("Here I go!");
-  console.log(pages);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  return <PageList pages={pages} />;
+  // Getting the search request and passing the data back
+  const { pages, hasResolved } = useSelect(
+    (select) => {
+      const query = {};
+
+      if (searchTerm) {
+        query.search = searchTerm;
+      }
+
+      const selectorArguments = ["postType", "page", query];
+
+      // making the requests to the WP store
+      const response = {
+        pages: select(coreDataStore).getEntityRecords(...selectorArguments),
+        hasResolved: select(coreDataStore).hasFinishedResolution("getEntityRecords", selectorArguments),
+      };
+
+      return response;
+    },
+    [searchTerm]
+  );
+
+
+
+  // the markup
+  return (
+    <div>
+      <SearchControl onChange={setSearchTerm} value={searchTerm} />
+      <PageList hasResolved={hasResolved} pages={pages} />
+    </div>
+  );
 }
 
-function PageList({ pages }) {
+function PageList({ hasResolved, pages }) {
+  if (!hasResolved) {
+    return <Spinner />;
+  }
+
+  if (!pages?.length) {
+    return <div>No results</div>;
+  }
+
   return (
-    <ul>
-      {pages?.map((page) => (
-        <li key={page.id}>{ decodeEntities( page.title.rendered ) }</li>
-      ))}
-    </ul>
+    <table className="wp-list-table widefat fixed striped table-view-list">
+      <thead>
+        <tr>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        {pages?.map((page) => (
+          <tr key={page.id}>
+            <td>{decodeEntities(page.title.rendered)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
